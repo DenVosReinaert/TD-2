078727f08641516eaf70926879f2bea70be054ba
Adriaan <adriaan.fazant@live.nl>
2021-07-18 13:00:21 +0200

    Temporary Component models
    *Removed the need for a base joint through the use of temporary component models which have their origin point set to the center of their base.

---
+++ Assets/Scripts/Components/Comp_Chasis.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comp_Chasis : MonoBehaviour
+{
+    #region IComponent interface requirements
+    public string componentType { get; set; }
+    public int attachmentPriority { get; set; }
+
+    public List<Transform> joints { get; set; }
+    #endregion
+
+    public string componentType_;
+    public int attachmentPriority_;
+
+    public List<Transform> joints_;
+
+    void Awake()
+    {
+        componentType = componentType_;
+        attachmentPriority = attachmentPriority_;
+        joints = joints_;
+
+        Debug.Log(componentType + ", " + attachmentPriority + ", " + joints[0].name);
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Interfaces/IComponent.cs
+++ Assets/Scripts/Interfaces/IComponent.cs
@@ -7,9 +7,5 @@ public interface IComponent
     public string componentType { get; set; }
     public int attachmentPriority { get; set; }
 
-
-    /*
-    * Note: Joints[0] should always be the joint where the component attaches to the joint of a component with a higher attachmentPriority (i.e. the base of the component).
-    */
     public List<Transform> joints { get; set; }
 }
